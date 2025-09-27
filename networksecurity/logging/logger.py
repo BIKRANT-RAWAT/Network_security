4ceec823d602dffc76a387c758f02f3d10a36d17
BIKRANT-RAWAT <connect.bikrantrawat@gmail.com>
2025-09-27 09:16:54 +0530

    logging and exception file ready

--- networksecurity/logging/logger.py
+++ networksecurity/logging/logger.py
@@ -4,9 +4,11 @@ from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
+#logfile path
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
+# complete path
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
